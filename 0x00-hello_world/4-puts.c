c50705f76fc61e2bdd2e4e7cf16a1b5406f18b48
kwasikwakye <kwasikwakye86@gmail.com>
2023-05-18 13:26:16 -0700

    c finnaly

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Print
+ *
+ * Return: Always 0 (Successsful)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
