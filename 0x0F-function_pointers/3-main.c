d2a5e96f42f4dabeabad6c842a2e02173ea7787b
kwasikwakye <kwasikwakye86@gmail.com>
2023-06-23 15:06:42 -0700

    sense

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - performs simple operations
+ * @argc: number of arguments passed
+ * @argv: array of pointers to arguments
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, c;
+	char o;
+	int (*f)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	f = get_op_func(argv[2]);
+	if (!f)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	o = *argv[2];
+	if ((o == '/' || o == '%') && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	c = f(a, b);
+	printf("%d\n", c);
+	return (0);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+coming like kakaye
